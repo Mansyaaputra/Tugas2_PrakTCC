496fb9dec3f8697348b4e45c99d31fcbd1c9cbbe
Mansyaaputra <ajipeterrrr@gmail.com>
2025-03-21 20:27:00 +0700

    update data

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('note','root','',{
-    host: 'localhost',
+const db = new Sequelize('note','root','bebas',{
+    host: '34.171.86.52',
     dialect: 'mysql',});
 
     export default db;
