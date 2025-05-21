73e479ebbef412af6f88dfaf0487337435ebce96
Mansyaaputra <ajipeterrrr@gmail.com>
2025-05-21 18:46:47 +0700

    update

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 import { createRoot } from "react-dom/client";
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+import App from './App';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
