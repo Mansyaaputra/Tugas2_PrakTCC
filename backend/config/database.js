b07ac5b7b368177dd7c0fc6fa41ebaac7849e6a3
Mansyaaputra <ajipeterrrr@gmail.com>
2025-05-21 17:11:59 +0700

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 
-const db = new Sequelize('note','root','',{
-    host: 'localhost',
-    dialect: 'mysql',});
+dotenv.config();
 
-    export default db;
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+export default db;
